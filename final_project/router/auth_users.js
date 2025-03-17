21440efb5f5751d1a253c779c0dfcd83e9e07f7d
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-03-17 18:59:45 -0400

    updated

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,6 +11,15 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+let validUsers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+})
+if(validUsers.length>0){
+    return true;
+}
+else{
+    return false;
+}
 }
 
 //only registered users can login
@@ -20,10 +29,23 @@ regd_users.post("/login", (req,res) => {
   const password = req.body.password;
   if(!username || !password)
   {
-    return req.status(404).json({message:"Error logging in"})
+    return req.status(400).json({message:"Error logging in"})
   }
-  return res.status(300).json({message: "Yet to be implemented"});
+  if(authenticatedUser(username,password)){
+    let accessToken = jwt.sign({
+        data:password
+    },'access',{expiresIn:60*60});
+    req.session.authorization ={
+        accessToken,username
+    }
+  
+  return res.status(200).send("user successfully logged in");
+  }
+  else{
+    return res.status(208).json({message:"Invalid Login.check username and password"})  
+}
 });
+
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
