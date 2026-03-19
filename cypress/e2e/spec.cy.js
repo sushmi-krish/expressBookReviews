61e0b088cebf55e2637a90cfd0e910bd05372098
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2026-03-19 19:46:14 +0000

    Cypress automation testing bug detected

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -16,7 +16,61 @@ describe('API Testing ',()=>{
         expect(response.body).to.be.an('object')
       })
     })
-    it('Post the review for the book',()=>{
-      cy.request('POST','http://localhost:5000/')
+    it('Register a new user',()=>{
+        cy.request({
+          method:'POST',
+          url:'http://localhost:5000/register',
+          failOnStatusCode: false,
+          body:{
+            "username":"testuser"+Date.now(),
+            "password":"testPassword"
+          }
+        }).then((response)=>
+        {
+          cy.log("Response Body:",JSON.stringify(response.body))
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.an('object')
+        })
     })
-})
+    it('Login with valid credentials',()=>{
+      const username = "testuser"+Date.now();
+      const password = "testPassword"
+      //register the user first 
+      cy.request({
+        method:'POST',
+        url:'http://localhost:5000/register',
+        failOnStatusCode: false,
+        body:{username,password}
+      }).then(()=>{
+        //login with the same credentials
+        cy.request({
+          method:'POST',
+          url:'http://localhost:5000/customer/login',
+          failOnStatusCode: false,
+          body:{username,password}
+        }).then((response)=>{
+          cy.log("Login Response Body:",JSON.stringify(response.body))
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.a('string')
+          expect(response.body).to.eq('user successfully logged in')
+        })
+      })
+      
+    })
+    it('validate login with invalid Credentials',()=>{
+      const username = "invalidUser"+ Date.now()
+      const password = "invalidPassword";
+      cy.request({
+          method:'POST',
+          url:'http://localhost:5000/customer/login',
+          failOnStatusCode:false,
+          body:{username,password}
+        }).then((response)=>{
+          cy.log("Login Response Body:",JSON.stringify(response.body));
+          expect(response.status).to.eq(400)
+          expect(response.body).to.be.a('string')
+          expect(response.body).to.eq('Invalid username or password')
+        })
+      })
+    })
+   
